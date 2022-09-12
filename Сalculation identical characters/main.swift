c367cbfe99091c62f7d5dab5824623c9df0cb569
Mykola <nickkosh@gmail.com>
2022-09-12 15:23:00 +0200

    1. bugfix 2. refactoring

--- Сalculation identical characters/main.swift
+++ Сalculation identical characters/main.swift
@@ -7,28 +7,39 @@
 
 //MARK: Task link
 // https://youtu.be/QLhqYNsPIVo?list=PL6Wui14DvQPySdPv5NUqV3i8sDbHkCKC5&t=923
-import Foundation
 
 // input any string
-let inputString: String? = "https://youtu.be/QLhqYNsPIVo?list=PL6Wui14DvQPySdPv5NUqV3i8sDbHkCKC5&t=923"
+let inputString: String? =  "https://youtu.be/QLhqYNsPIVo?list=PL6Wui14DvQPySdPv5NUqV3i8sDbHkCKC5&t=923"
 
 // dictionary to save values
 var dict = [String: Int]()
+var resultChar = ""
+var resultNum = 0
 
-// check to nil
+// check string to nil
 if inputString != nil {
-    // string to lowercase type
-    if let inputUnwrapString = inputString?.lowercased() {
+    
+    // Use Optional Binding and convert string to uppercased type
+    if let inputUnwrapString = inputString?.uppercased() {
+        
         // loop for saving value
-        for now in inputUnwrapString {
-            let key = String(now)
-            if dict.keys.contains(key) {
-                dict[key]! += 1
-            } else {
+        for char in inputUnwrapString {
+            let key = String(char)
+            
+            // check value in dictionary ? add value : plus value
+            if dict.keys.contains(key) == false {
                 dict[key] = 1
+            } else {
+                if dict[key] != nil {
+                    dict[key]! += 1
+                    if resultNum < dict[key]! {
+                        resultNum = dict[key]!
+                        resultChar = key
+                    }
+                }
             }
         }
     }
 }
 
-print(dict.values.max()!)
+print("In the string most often, exactly \(resultNum) times there is a character \(resultChar)")
